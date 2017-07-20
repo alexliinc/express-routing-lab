f1c01d938342c68510e0522865d44abdd2857710
alexliinc <alexli@indiana.edu>
2017-07-20 17:16:38 -0600

    complete

--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -11,6 +11,16 @@ var candies = [{
     id: 2,
     name: "lolly",
     color: "blue"
+  },
+  {
+    id: 3,
+    name: "pop",
+    color: "green"
+  },
+  {
+    id: 4,
+    name: "sod",
+    color: "black"
   }
 ];
 //bodyParser.json()
@@ -27,28 +37,44 @@ router.get('/', function(req, res) {
 // Fill out the rest of the routes here
 
 router.get('/:id', function(req, res) {
-  res.send("YOU MADE IT to /:id");
+  //console.log(req);
+  // //res.send("YOU MADE IT to /:id with :" + id);
+  var current = candies[req.params.id - 1];
+  res.json(current);
 });
 
-// router.get('/new', function(req, res) {
-//   res.send("YOU MADE IT to /NEW");
-// });
-//
-// router.post('/', function(req, res) {
-//   res.send("YOU MADE IT to /post");
-// });
-//
-// router.get('/:id/edit', function(req, res) {
-//   res.send("YOU MADE IT to /get id/EDIT");
-// });
-//
-// router.put('/:id/', function(req, res) {
-//   res.send("YOU MADE IT to /get id/PUT");
-// });
-//
-// router.delete('/:id', function(req, res) {
-//   res.send("YOU MADE IT to /get id/DELETE");
-// });
+router.post('/', function(req, res) {
+  //res.send("YOU MADE IT to /post");
+  // var id = req.body.id;
+  // var name = req.body.name;
+  // var color = req.body.color;
+  // var newCandy = {
+  //   id: id,
+  //   name: name,
+  //   color: color
+  // };
+  // candies.push(newCandy);
 
+  candies.push(req.body);
+  console.log(req.body);
+  res.send("posted");
+  //res.send(id + name + color);
+});
+
+router.put('/:id/', function(req, res) {
+  var current = req.params.id - 1;
+  candies[current].id = req.body.id;
+  candies[current].name = req.body.name;
+  candies[current].color = req.body.color;
+  //res.send("YOU MADE IT to /get id/PUT");
+  res.send("you made a put");
+});
+
+router.delete('/:id', function(req, res) {
+  //res.send("YOU MADE IT to /get id/DELETE");
+  var current = req.params.id - 1;
+  candies[current] = null;
+  res.send("Removed" + current);
+});
 
 module.exports = router;
